138d9723decb929bfedc7362006923eee5cef166
田玉星 <624239110@qq.com>
2025-03-05 19:08:21 +0800

    修改测试文件

--- example/test.php
+++ example/test.php
@@ -2,14 +2,14 @@
 require __DIR__.'/../vendor/autoload.php';
 use Scimall\Acad\Client;
 // 配置参数
-$appKey ='5523c186';
-$apiUrl = 'http://tyx.acads_sy.scimall.vip/open-api/api.php';
+$appKey ='xxxxxxx';
+$apiUrl = '联系管理员';
 // 初始化客户端
 $client = new Client($appKey, $apiUrl);
 try {
     // 示例1：获取用户身份信息
     $params = [
-        'phone' => '13548578246',
+        'phone' => 'xxxxxx',
         'email' => 'test@example.com'
     ];
     $result = $client->getUserIdentity($params);
